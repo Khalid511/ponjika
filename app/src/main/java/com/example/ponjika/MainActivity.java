87780b76384380b07e46086ce8ef6437009829e4
Md. Khalid Hasan <shohag9438@gmail.com>
2022-09-14 07:10:29 +0600

    add date listener to show date, and a event button corresponding that date

--- app/src/main/java/com/example/ponjika/MainActivity.java
+++ app/src/main/java/com/example/ponjika/MainActivity.java
@@ -1,11 +1,19 @@
 package com.example.ponjika;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.Window;
+import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textview;
+    CalendarView calendarview;
+    Button eventButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +21,18 @@ public class MainActivity extends AppCompatActivity {
        // requestWindowFeature(Window.FEATURE_NO_TITLE);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
+
+        textview = findViewById(R.id.textView);
+        calendarview= findViewById(R.id.calendarView);
+        eventButton = findViewById(R.id.event_button);
+
+        calendarview.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                String date =  "Clicked date\n"+String.valueOf(dayOfMonth) + "-" + String.valueOf(month) + "-" + String.valueOf(year);
+                textview.setText(date);
+                eventButton.setVisibility(view.VISIBLE);
+            }
+        });
     }
 }
